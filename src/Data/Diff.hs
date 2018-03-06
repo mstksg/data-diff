615f0bf51de5606bce7dd5865ce131012e729e3f
Justin Le <justin@jle.im>
2018-03-06 15:27:44 -0800

    re-export list in data.diff

--- src/Data/Diff.hs
+++ src/Data/Diff.hs
@@ -1,8 +1,34 @@
 {-# LANGUAGE TypeFamilies         #-}
 
 module Data.Diff (
-    module D
+    Diff(..)
+  , Patch(..), DiffLevel(..), MergeResult(..)
+  , compareDiff, noDiff
+  , Edit'(..), diff', patch'
+  , TuplePatch(..)
+  , EitherPatch(..)
+  -- * Generic implementations
+  -- ** Patch
+  , gpatchLevel
+  , gmergePatch
+  -- ** Diff
+  -- *** Sequences
+  , listDiff
+  , listPatch
+  , SeqPatch(..)
+  , seqDiff
+  , seqPatch
+  -- *** Generic ADTs
+  , GPatch(..)
+  , gdiff
+  , gdiff'
+  , gpatch
+  , GPatchProd(..)
+  , gdiffProd
+  , gpatchProd
+  , SumDiff(..)
   ) where
 
-import           Data.Diff.Internal as D
-import           Data.Diff.Sequence as D
+import           Data.Diff.Internal
+import           Data.Diff.Generics
+import           Data.Diff.Sequence
