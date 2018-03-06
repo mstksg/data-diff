3f985f8ecaf341a4ab789b357bcaabcf45ded187
Justin Le <justin@jle.im>
2018-03-06 00:57:17 -0800

    Update gh-pages at Tue Mar  6 00:57:17 PST 2018

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# Changelog for generics-diff
-
-## Unreleased changes
--- src/Data/Diff.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE TypeFamilies         #-}
-
-module Data.Diff (
-    module D
-  ) where
-
-import           Data.Diff.Internal as D
-import           Data.Diff.Sequence as D
