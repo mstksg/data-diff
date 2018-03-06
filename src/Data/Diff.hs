8bc187afb1eab352525196cfe1a6d69829cbc29c
Justin Le <justin@jle.im>
2018-03-05 16:23:08 -0800

    patching functions

--- src/Data/Diff.hs
+++ src/Data/Diff.hs
@@ -1,11 +1,20 @@
-{-# LANGUAGE TypeFamilies            #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies     #-}
 
 module Data.Diff (
     Diff(..)
   , Edit'(..), diff', patch'
   ) where
 
-class Eq a => Diff a where
+data DiffLevel = NoDiff
+               | PartialDiff
+               | TotalDiff
+    deriving (Eq, Ord, Show)
+
+class Patch a where
+    patchLevel :: a -> DiffLevel
+
+class Patch (Edit a) => Diff a where
     type Edit a
     diff      :: a -> a -> Edit a
     patch     :: Edit a -> a -> Maybe a
@@ -18,18 +27,34 @@ diff' x y = Edit' (diff x y)
 patch' :: Diff a => Edit' a -> a -> Maybe a
 patch' (Edit' x) = patch x
 
-instance (Diff a, Diff b) => Diff (a, b) where
-    type Edit (a, b) = (Edit a, Edit b)
-    diff (x1, y1) (x2, y2) = (diff x1 x2, diff y1 y2)
-    patch (ex, ey) (x, y) = (,) <$> patch ex x <*> patch ey y
+data TuplePatch a b = TP (Edit a) (Edit b)
 
-data EitherEdit a b = L2L (Edit a)
-                    | L2R a b
-                    | R2L b a
-                    | R2R (Edit b)
+instance (Patch (Edit a), Patch (Edit b)) => Patch (TuplePatch a b) where
+    patchLevel (TP x y) = case (patchLevel x, patchLevel y) of
+      (NoDiff     , NoDiff   ) -> NoDiff
+      (NoDiff     , _        ) -> PartialDiff
+      (PartialDiff, _        ) -> PartialDiff
+      (TotalDiff  , TotalDiff) -> TotalDiff
+      (TotalDiff  , _        ) -> PartialDiff
+
+instance (Diff a, Diff b) => Diff (a, b) where
+    type Edit (a, b)         = TuplePatch a b
+    diff (x1, y1) (x2, y2)   = TP (diff x1 x2) (diff y1 y2)
+    patch (TP ex ey) (x, y)  = (,) <$> patch ex x <*> patch ey y
+
+data EitherPatch a b = L2L (Edit a)
+                     | L2R a b
+                     | R2L b a
+                     | R2R (Edit b)
+
+instance (Patch (Edit a), Patch (Edit b)) => Patch (EitherPatch a b) where
+    patchLevel (L2L e  ) = patchLevel e
+    patchLevel (L2R _ _) = TotalDiff
+    patchLevel (R2L _ _) = TotalDiff
+    patchLevel (R2R e  ) = patchLevel e
 
 instance (Diff a, Diff b) => Diff (Either a b) where
-    type Edit (Either a b) = EitherEdit a b
+    type Edit (Either a b) = EitherPatch a b
     diff (Left  x) (Left  y) = L2L (diff x y)
     diff (Left  x) (Right y) = L2R x y
     diff (Right x) (Left  y) = R2L x y
